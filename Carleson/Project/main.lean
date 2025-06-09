1e49b7fe19972e1b5d5cabb4b6d2245880c16e24
izamandla <s36imand@uni-bonn.de>
2025-06-09 12:34:58 +0200

    trying to do some stuff with mainresult

--- Carleson/Project/main.lean
+++ Carleson/Project/main.lean
@@ -4,5 +4,5 @@ import Carleson.Project.modules.Utiles
 
 theorem mainresult (N : ℕ) (f : ℝ → ℝ) (x : ℝ) :
   Walsh.walshFourierPartialSum f N x = (∫ y in Set.Icc 0 1, f y * Walsh.walsh N y * Walsh.walsh N x * Kernel.kernel N x y) := by
-
+  rw[Utiles.partition]
   sorry
