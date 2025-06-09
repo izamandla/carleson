f131822cb85594841fc0093542b27bf38eb1fcaa
izamandla <s36imand@uni-bonn.de>
2025-06-09 10:20:56 +0200

    ?

--- Carleson/Project/main.lean
+++ Carleson/Project/main.lean
@@ -1,7 +1,8 @@
-import LeanCourse.Project.modules.Utiles
+import Carleson.Project.modules.Utiles
 
 /- ## Main result -/
 
 theorem mainresult (N : ℕ) (f : ℝ → ℝ) (x : ℝ) :
   Walsh.walshFourierPartialSum f N x = (∫ y in Set.Icc 0 1, f y * Walsh.walsh N y * Walsh.walsh N x * Kernel.kernel N x y) := by
+
   sorry
